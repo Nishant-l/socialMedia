1df2f34662be94db578935325bb97daf41a5aa19
Nishant <nishant3151998@gmail.com>
2020-12-09 17:32:31 +0530

    🎨Setting up static engine

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const expressEjsLayouts = require('express-ejs-layouts');
 const app = express();
 const port=8000;
 
+app.use(express.static('./assets'));
 // to use ejs layouts
 app.use(expressEjsLayouts);
+// extract style and script from sub pages into layouts
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 // use express router
 app.use('/',require('./routes'));
