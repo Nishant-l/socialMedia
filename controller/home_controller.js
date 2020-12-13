9dcc856cc44cbbcbb31d3e8b9e799db4f4b7f18f
Nishant <nishant3151998@gmail.com>
2020-12-13 23:24:02 +0530

    🍪 setting up for authantication(cookies)

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,4 +1,5 @@
 module.exports.home=function(req,res){
     // return res.end('<h1>hii</h1>');
+    console.log(req.cookies);
     return res.render('home.ejs',{title:'homePage'});
 }
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cookieParser=require('cookie-parser');
 const expressEjsLayouts = require('express-ejs-layouts');
 const app = express();
 const port=8000;
 const db=require('./config/mongoose')
+
+app.use(express.urlencoded());
+app.use(cookieParser());
 
 app.use(express.static('./assets'));
 // to use ejs layouts
