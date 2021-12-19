f3307e172c9fc82a6bc8a330b29a53ab09c8e3ba
Nishant-l <nishant3151998@gmail.com>
2021-12-20 01:01:19 +0530

    ✍️ :Created model for Post;

---
+++ model/post.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const postSchama = new mongoose.Schema({
+    comment: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+},{
+    timestamps: true
+});
+
+const Post = mongoose.model('Post',postSchama);
+
+module.exports = Post;
